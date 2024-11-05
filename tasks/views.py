9d49a6595159c2a5acde3a66bcad31b9201a474d
hajra7 <hajrasultana707@gmail.com>
2024-11-05 16:38:14 +0900

    Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,42 @@
+# tasks/views.py
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+# View to list tasks
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+# View to add a new task
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
+
+# View to edit a task
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/edit_task.html', {'form': form, 'task': task})
+
+# View to delete a task
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/delete_task.html', {'task': task})
+
